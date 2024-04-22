909648dcf2c3b99194d5dfba46783dda492e02cf
TheBenster <bbeaver93@gmail.com>
2024-04-22 15:47:11 -0400

    inspired nav bar but for category

---
+++ src/components/CategoryBar.jsx
@@ -0,0 +1,19 @@
+import useFetch from "../hooks/useFetch";
+
+const CategoryBar = () => {
+    const categoryURL = 'https://fakestoreapi.com/products/categories'
+    const categoryJSON = useFetch(categoryURL)
+    const categories = categoryJSON.data
+
+    return (
+        <div className=" flex justify-between border-t-2 border-b-2 p-3">
+            <hr className=" " />
+            {categories.map((category) => <p className=" hover:text-sky-900 hover:cursor-pointer">{category}</p>)}
+            <hr/>
+        </div>
+    )
+
+
+}
+
+export default CategoryBar
