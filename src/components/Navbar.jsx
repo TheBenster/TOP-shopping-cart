95479e47654df3b3a657ccb2e822aea5f4a79e06
TheBenster <bbeaver93@gmail.com>
2024-04-22 15:48:00 -0400

    shopping cart icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,18 @@
+import cart from '../assets/cart.png'
+import CategoryBar from './CategoryBar'
+
 const Navbar = () => {
     return (
-        <div className="w-full pl-8 pt-4 flex justify-between align-start">
-            <div>Logo goes here</div>
-            <div id='nav-links' className="pr-8 flex gap-3">
-                <a href="" className="text-xl text-gray-900 hover:text-sky-950">Home</a>
-                <a href="" className="text-xl text-gray-900 hover:text-sky-950">Shop</a>
-                <a href="" className="text-xl text-gray-900 hover:text-sky-950">Learn More</a>
+        <div className="w-full pl-8 pt-4 flex justify-between  align-start">
+            <div className='content-center'>Logo goes here</div>
+            <div id='nav-links' className="pr-8 flex gap-3 ">
+                <a href="" className="text-xl content-center text-gray-900 hover:text-sky-950">Home</a>
+                <a href="" className="text-xl content-center text-gray-900 hover:text-sky-950">Shop</a>
+                <div id="cart" className='w-12 content-center h-12 object-center'>
+                <img src={cart} alt="" />
+                </div>
             </div>
+            
         </div>
     )
 }
