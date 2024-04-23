c3b0bddb343946feebf2f51500f5cc9ee906dcf0
TheBenster <bbeaver93@gmail.com>
2024-04-23 12:37:37 -0400

    change to router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/HeroHome.jsx'
 import './App.css'
+import CategoryBar from './components/CategoryBar.jsx'
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/routes.jsx'
+import { Outlet } from 'react-router-dom';
+
+// import  { useFetch } from './hooks/useFetch.jsx'
 
 function App() {
-
+  // const { data, isLoading, error } = useFetch();
   return (
-    <>
-      <Navbar />
-      <Hero />
-    </>
+    <RouterProvider router={router}/>
   )
 }
 
