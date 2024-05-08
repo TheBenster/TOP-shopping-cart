6948cea7866912a13868ac04be98f5d91d9b2b89
TheBenster <bbeaver93@gmail.com>
2024-05-08 12:38:45 -0400

    add commented section where interactivity should be for when addin to cart

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import cart from '../assets/cart.png'
 import CategoryBar from './CategoryBar'
 import Shop from './Shop'
 import { Link } from 'react-router-dom'
+import Cart from './Cart'
 
 const Navbar = () => {
     return (
@@ -11,7 +12,7 @@ const Navbar = () => {
                 <Link to="/" className="text-xl content-center text-gray-900 hover:text-sky-950">Home</Link>
                 <Link to="shop" className="text-xl content-center text-gray-900 hover:text-sky-950">Shop</Link>
                 <div id="cart" className='w-12 content-center h-12 object-center'>
-                <img className='hover:text-sky-950 hover:cursor-pointer' src={cart} alt="" />
+                    <div className=' flex ' ><Link to='/cart'><img className='hover:text-sky-950 hover:cursor-pointer' src={cart} alt="" /></Link> {/* put length here*/}</div>      
                 </div>
             </div>
         </div>
