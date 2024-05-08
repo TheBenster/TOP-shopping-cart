4a83fc0aab5aca5e009952c8680daa8e993ef535
TheBenster <bbeaver93@gmail.com>
2024-05-08 16:59:36 -0400

    add logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,17 @@ import CategoryBar from './CategoryBar'
 import Shop from './Shop'
 import { Link } from 'react-router-dom'
 import Cart from './Cart'
+import lgo from '../assets/lgo.png'
 
-const Navbar = () => {
+const Navbar = ({shoppingCart}) => {
     return (
         <div className="w-full pl-8 pt-4 flex justify-between sticky align-start">
-            <div className='content-center'>Logo goes here</div>
+            <div className='content-center flex align-middle items-center'><img className='w-14' src={lgo} alt="" />Hex</div>
             <div id='nav-links' className="pr-8 flex gap-3 ">
                 <Link to="/" className="text-xl content-center text-gray-900 hover:text-sky-950">Home</Link>
                 <Link to="shop" className="text-xl content-center text-gray-900 hover:text-sky-950">Shop</Link>
                 <div id="cart" className='w-12 content-center h-12 object-center'>
-                    <div className=' flex ' ><Link to='/cart'><img className='hover:text-sky-950 hover:cursor-pointer' src={cart} alt="" /></Link> {/* put length here*/}</div>      
+                    <div className=' flex ' ><Link to='/cart'><img className='hover:text-sky-950 hover:cursor-pointer' src={cart} alt="" /></Link>  {shoppingCart.length < 1 ? null : (shoppingCart.length) }</div>      
                 </div>
             </div>
         </div>
