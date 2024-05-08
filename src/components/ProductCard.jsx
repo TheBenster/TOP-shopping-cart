548abd59d6e38b9878f568248e54c610d16be558
TheBenster <bbeaver93@gmail.com>
2024-05-08 16:58:58 -0400

    add footer, aadd logo, add toFixed.

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -12,7 +12,7 @@ const ProductCard = ({title, description, price, image, productId, handleAddToCa
             <img className="mx-auto my-auto max-w-full max-h-full pt-6" src={image} alt="product-image" />
             </div>
             <div className="flex flex-row justify-between w-full mt-2 gap-4 pt-5">
-                <p className="flex justify-center text-center align-center items-center flex-grow font-semibold "><span className="font-bold pr-2 text-2xl">$ </span> {price} </p>
+                <p className="flex justify-center text-center align-center items-center flex-grow font-semibold "><span className="font-bold pr-2 text-2xl">$ </span> {price.toFixed(2)} </p>
                             
             </div>
         </div>
