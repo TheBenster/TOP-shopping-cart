17c7e69ffdfdbe99dd368ebb3acf8cd056389737
TheBenster <bbeaver93@gmail.com>
2024-05-08 12:36:43 -0400

    put shoppingCart prop in parent component, leaving child to handle event :3

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Navbar from './Navbar';
 import { Outlet } from 'react-router-dom';
+import { useState } from 'react';
 
+// This will contain the navbar under every circumstance. Everything else is an outlet, and thus context.
 const Root = () => {
+    const [shoppingCart, setShoppingCart] = useState([]);
+    const shoppingCartLength = Object.keys(shoppingCart).length;
+    const [quantity, setQuantity] = useState(1);
+
     return (
         <>
-            <Navbar />
-            <Outlet />
+            <Navbar  />
+            <Outlet context={{shoppingCart, setShoppingCart, quantity, setQuantity}} />
         </>
     );
 };
