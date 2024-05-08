8904e2f9b3b0ac2df239f54c1c3b4730f6d079f9
TheBenster <bbeaver93@gmail.com>
2024-05-08 16:57:44 -0400

    toggle

--- src/components/CategoryBar.jsx
+++ src/components/CategoryBar.jsx
@@ -1,35 +1,38 @@
 import useFetch from "../hooks/useFetch";
-import { useEffect, useState } from 'react';
+import { useState } from "react";
 
-
-const CategoryBar = () => {
-    const categoryURL = 'https://fakestoreapi.com/products/categories';
+const CategoryBar = ({ onCategoryClick }) => {
+    const categoryURL = "https://fakestoreapi.com/products/categories";
     const categoryJSON = useFetch(categoryURL);
     const categories = categoryJSON.data;
-    const [selectedFilters, setSelectedFilters] =useState([])
+    const [selectedCategory, setSelectedCategory] = useState(null);
 
-    const handleClick = (e) => {
-        const clickedCategory = e.target.textContent;
-        if(!selectedFilters.includes(clickedCategory)){
-            setSelectedFilters((prevFilters) => {
-                const newFilter = [...prevFilters, clickedCategory];
-                console.log('array items: ' + newFilter);
-                
-                return newFilter
-            })           
+    const handleClick = (category) => {
+        if (selectedCategory === category) {
+            setSelectedCategory(null);
+        } else {
+            setSelectedCategory(category);
         }
-    }
-    
+        onCategoryClick(category);
+    };
 
     return (
         <div className=" flex justify-between border-t-2 border-b-2 p-3">
             <hr className=" " />
-            {categories.map((category) => <p onClick={handleClick} key={category.id} className=" hover:text-sky-900 hover:cursor-pointer ">{category}</p>)}
-            <hr/>
+            {categories.map((category) => (
+                <p
+                    onClick={() => handleClick(category)}
+                    key={category}
+                    className={`hover:text-sky-900  hover:cursor-pointer ${
+                        selectedCategory === category ? "text-sky-900 font-bold" : ""
+                    }`}
+                >
+                    {category}
+                </p>
+            ))}
+            <hr />
         </div>
-    )
+    );
+};
 
-
-}
-
-export default CategoryBar
+export default CategoryBar;
